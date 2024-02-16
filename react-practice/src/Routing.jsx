49bf4574873503c6061519267810423869113d0d
mahaboob-basha-a <alurumahaboobbasha@gamil.com>
2024-02-16 17:47:58 +0530

    second commit

--- react-practice/src/Routing.jsx
+++ react-practice/src/Routing.jsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom';
 
 function Routing(){
     return(<div>
-        <ul className='d-flex'>
+        <ul className='bg-warning d-flex'>
           <li className='ml-4'><Link to="/home">Home</Link></li>
           <li className='ml-5'><Link to={'/about'}>About</Link></li>
           <li className='ml-5'><Link to={'/login'}>Login</Link></li>
--- react-practice/src/login.jsx
+++ react-practice/src/login.jsx
@@ -1,12 +1,16 @@
 const Login = ()=>{
+    function para(){
+        return alert("hi");
+    }
     return(<div className="p-4">
         <form>
             <label className="text-dark" htmlFor="nameOne">Name</label>
             <input type="text" name="" id="nameOne" className="form-control" />
             <label className="text-dark" htmlFor="nameOne">Password</label>
             <input type="text" name="" id="nameOne" className="form-control" />
-            <button className="btn btn-primary mt-4">Login Now</button>
+            <button className="btn btn-primary mt-4" onClick={para}>Login Now</button>
         </form>
+        <p></p>
     </div>);
 }
 
