a44673c0ccbbc4bbc6560da70d33c588c3b7f02d
mahaboob-basha-a <alurumahaboobbasha@gamil.com>
2024-03-01 07:33:13 +0530

    drinks api link updating

--- react-practice/src/Drinks.jsx
+++ react-practice/src/Drinks.jsx
@@ -1,22 +1,27 @@
 import {useState,useEffect} from 'react';
 import './coctail.css';
 
-let URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+let URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
 
 
 export const Drinks = ()=> {
     const [value,setvalue] = useState('');
     const [arr,setarr] = useState([]);
-    const getting = async (url)=>{
+    const getting = async (urls)=>{
+        try{
 
-        let apiV = await fetch(url+value);
-        let {drinks} = await apiV.json();
-        setarr(drinks);
+            let apiV = await fetch(urls);
+            let {drinks} = await apiV.json();
+            setarr(drinks);
+        }catch(e){
+            console.log(e);
+        }
     }
 
     useEffect(()=>{
-        getting(URL)
+        let finalUrl = `${URL}${value}`
+        getting(finalUrl)
     },[value]);
     return (<>
         <div>
@@ -24,13 +29,14 @@ export const Drinks = ()=> {
         <hr />
         <ul>
             {
+                arr.length > 0 ?
                 arr.map((item)=>{
                     const {idDrink,strDrink,strDrinkThumb} = item;
                     return <li key={idDrink}>
                         <img src={strDrinkThumb} alt={strDrink} />
                         <h4>{strDrink}</h4>
                     </li>
-                })
+                }):<h1>No data found</h1>
             }
         </ul>
         </div>
